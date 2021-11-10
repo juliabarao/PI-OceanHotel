1d8b4cf2172f12f158cc0e7812d0c020fa3e0641
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-11-10 19:24:13 -0300

    update, fixei navbar e estilizei
    usei o jquery pra por uns efeitin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-carousel = (function(){
+carousel = (function() {
     var carr = document.querySelector('.carrousel');
     var next = document.querySelector('.next');
     var prev = document.querySelector('.prev');
@@ -10,16 +10,16 @@ carousel = (function(){
     var itemsLength = items.length;
     var current = items[0];
     var myVar;
-    
-    
+
+
     function slide(direction) {
         current.classList.remove('current');
         currentCircle.classList.remove('active');
         counter = counter + direction;
-        if(direction === -1 && counter < 0 ) {
+        if (direction === -1 && counter < 0) {
             counter = itemsLength - 1;
         }
-        if(direction === 1 && !items[counter]) {
+        if (direction === 1 && !items[counter]) {
             counter = 0;
         }
         current = items[counter];
@@ -28,30 +28,30 @@ carousel = (function(){
         currentCircle.classList.add('active');
         console.log("Slide Called!")
     }
-    
+
     next.addEventListener('click', function() {
         slide(1);
     });
-    
+
     prev.addEventListener('click', function() {
         slide(-1);
     });
-    
+
     function sliding() {
         myVar = setInterval(function() {
             slide(1);
         }, 5000);
     };
     sliding();
-    
+
     carr.addEventListener('mouseover', function() {
         clearInterval(myVar);
     });
-    
-    
+
+
     carr.addEventListener('mouseleave', function() {
         console.log("moseleaved!")
         sliding();
     });
-    
+
 })();
